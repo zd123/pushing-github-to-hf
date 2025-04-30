501669a817f59ef6d8009797711bed42ea3c61ae
zd123 <zacharydesario@gmail.com>
2025-04-30 17:22:25 -0400

    adding app files

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# pushing-github-to-hf
-pushing-github-to-hf
+---
+title: Pushing Github To Hf
+emoji: 📚
+colorFrom: red
+colorTo: indigo
+sdk: gradio
+sdk_version: 5.28.0
+app_file: app.py
+pinned: false
+short_description: pushing-github-to-hf
+---
+
---
+++ app.py
@@ -0,0 +1,46 @@
+import streamlit as st
+from huggingface_hub import InferenceClient
+
+
+# MUST SET HF_TOKEN IN STREAMLIT SETTINGS IN HUGGINGFACE REPO SECRETS
+HF_TOKEN = st.secrets["HF_TOKEN"]
+
+
+# INIT THE INFERENCE CLIENT WITH YOUR HF TOKEN
+client = InferenceClient(
+    provider="fireworks-ai",
+    api_key=HF_TOKEN,
+)
+
+# THIS IS JUST THE streamlit TEXT INPUT WIDGET
+user_input = st.text_input(
+    "Place your prompt here",
+    "This is a placeholder",
+    key="placeholder",
+)
+
+# THIS IS THE INFERENCE CLIENT CALL
+completion = client.chat.completions.create(
+    model="deepseek-ai/DeepSeek-R1",
+    messages=[
+        {
+            "role": "user",
+            "content": user_input
+        }
+    ],
+    max_tokens=512,
+)
+
+# THIS IS THE RESPONSE FROM THE INFERENCE CLIENT
+ai_response = completion.choices[0].message.content
+
+# THIS IS THE STREAMLIT TEXT OUTPUT WIDGET WITH THE RESPONSE FROM THE INFERENCE CLIENT
+st.text(ai_response)
+
+
+### WRONG WAY TO TRY AND LOAD MODELS::: 
+# Load model directly
+# from transformers import AutoTokenizer, AutoModelForCausalLM
+
+# tokenizer = AutoTokenizer.from_pretrained("deepseek-ai/DeepSeek-Prover-V2-671B", trust_remote_code=True)
+# model = AutoModelForCausalLM.from_pretrained("deepseek-ai/DeepSeek-Prover-V2-671B", trust_remote_code=True)
